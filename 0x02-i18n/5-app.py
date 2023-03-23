1ed6c0122e16cdbae95817113c3c22e0d4899426
Alatise Oluwaseun <holynation667@gmail.com>
2023-03-23 18:44:50 +0100

    fix: Corrected the error code

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
-'''
-Basic flask app with babel setup
-'''
-from flask import Flask, g, render_template, request
-from flask_babel import Babel, _
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
+from flask_babel import Babel
 
 
-class Config(object):
-    '''
-        Config Class
-    '''
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -26,48 +19,58 @@ users = {
 }
 
 
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_user():
+    """
+    Returns a user dictionary or None if ID value can't be found
+    or if 'login_as' URL parameter was not found
+    """
+    id = request.args.get('login_as', None)
+    if id is not None and int(id) in users.keys():
+        return users.get(int(id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Add user to flask.g if user is found
+    """
+    user = get_user()
+    g.user = user
+
+
 @babel.localeselector
 def get_locale():
-    '''Gets best match locale according to request'''
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-    if g.user:
-        locale = g.user.get('locale')
-        if locale in app.config['LANGUAGES']:
-            return locale
-
-    locale = request.headers.get('locale')
+    """
+    Select and return best language match based on supported languages
+    """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    '''
-        Index page
-    '''
+    """
+    Handles / route
+    """
     return render_template('5-index.html')
 
 
-def get_user():
-    '''
-        function that returns a user dictionary
-        or None if the ID cannot be found or if login_as was not passed
-    '''
-    user_id = request.args.get('login_as')
-
-    if user_id is None:
-        return None
-
-    return users.get(int(user_id))
-
-
-@app.before_request
-def before_request():
-    """sets a user object to flask.g"""
-    user = get_user()
-    g.user = user
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
